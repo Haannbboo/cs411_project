c4ba68f9874a741036348fd6fdb13722b87d2a91
davidxiong <daweix3@illinois.edu>
2021-08-02 20:05:01 +0800

    Create MainPage.py
    add the main page

---
+++ MainPage.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from flask import Flask, render_template, request, url_for,redirect,session, make_response, Response,config
+import os
+
+app = Flask(__name__)
+# run the app connecting with the flask
+app.config['SECRET_KEY'] = os.urandom(24)
+
+
+
+Base = declarative_base()
+engine1 = create_engine('mysql+pymysql://root:davidxiong@localhost:3306/Airports')
+DBSession = sessionmaker(bind = engine1)
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    port = 3306,
+    user = 'root',
+    passwd = 'david0811',
+    database = 'Airports',)
+
+mycursor = mydb.cursor()
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
