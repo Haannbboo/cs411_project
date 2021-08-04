b6682c9ad318bf80be8709d2e8c25b9d3a06e02c
Hanbo <wanliuguohanbo@163.com>
2021-08-04 13:25:29 +0800

    Manager trigger added; User partially completed

--- Manager.py
+++ Manager.py
@@ -9,7 +9,7 @@ mydb = pymysql.connect(
     host='localhost',
     port=3306,
     user='root',
-    passwd='',
+    passwd='veGryq-roccab-tawsi5',
     database='test', )
 
 mycursor = mydb.cursor()
@@ -22,6 +22,28 @@ mycursor = mydb.cursor()
 # Base = declarative_base()
 # engine1 = create_engine('mysql+pymysql://root:david0811@localhost:3306/Airports')
 # DBSession = sessionmaker(bind=engine1)
+
+try:
+    SQL = "DROP TRIGGER insertTrig;"
+    mycursor.execute(SQL)
+except pymysql.err.OperationalError:
+    pass
+# SET @flight = (SELECT id FROM flights WHERE FlightNumber = new.FlightNumber AND deptTime = new.deptTime);
+insertTrig = """
+CREATE TRIGGER insertTrig
+    BEFORE INSERT ON flights
+    FOR EACH ROW
+    BEGIN
+        IF EXISTS(SELECT id FROM flights WHERE FlightNumber = new.FlightNumber AND deptTime = new.deptTime) THEN
+            SIGNAL SQLSTATE '45000';
+        END IF;
+        IF @flight IS NULL THEN
+            SET @maxID = (SELECT MAX(id) FROM flights);
+            SET new.id = @maxID + 1;
+        END IF;
+    END;
+"""
+mycursor.execute(insertTrig)
 
 
 class Manager():
@@ -43,10 +65,10 @@ class Manager():
 
     def add_flight(self, form: dict):
         cursor = mydb.cursor()
-        cursor.execute("SELECT MAX(id) FROM flights")
-        max_id = cursor.fetchone()[0]
+        # cursor.execute("SELECT MAX(id) FROM flights")
+        # max_id = cursor.fetchone()[0]
 
-        fid = max_id + 1
+        # fid = max_id + 1
         FlightNumber = form.get("FlightNumber", "")
         Airplane = form.get("Airplane", "")
         Airline = form.get("Airline", "")
@@ -57,10 +79,25 @@ class Manager():
         Price = form.get("Price", "")
         AvailableSeats = form.get("AvailableSeats", 0)
 
-        SQL = "INSERT INTO flights VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {}, {})".format(FlightNumber, Airplane, Airline, deptTime, deptAirport, arrTime, arrAirport, Price, AvailableSeats, fid)
+        SQL = "INSERT INTO flights VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {}, 0)".format(FlightNumber,
+                                                                                                           Airplane,
+                                                                                                           Airline,
+                                                                                                           deptTime,
+                                                                                                           deptAirport,
+                                                                                                           arrTime,
+                                                                                                           arrAirport,
+                                                                                                           Price,
+                                                                                                           AvailableSeats,)
         print(SQL)
-        cursor.execute(SQL)
-        mydb.commit()
+        try:
+            cursor.execute(SQL)
+            mydb.commit()
+            return True
+        except pymysql.err.OperationalError as e:
+            if e.args[0] == 1644:  # signal 45000
+                return False
+            else:
+                print(e)
 
     def edit_flight(self, fid: int, form: dict):
         # Input: request.form
@@ -81,13 +118,18 @@ class Manager():
         Price = form.get("Price", flight.get("Price"))
         AvailableSeats = form.get("AvailableSeats", flight.get("AvailableSeats"))
 
-        SQL = "UPDATE flights SET FlightNumber = '{FlightNumber}', Airplane = '{Airplane}', Airline = '{Airline}', deptTime = '{deptTime}', deptAirport = '{deptAirport}', arrTime = '{arrTime}', arrAirport = '{arrAirport}', Price = '{Price}', AvailableSeats = {AvailableSeats} WHERE id = {fid};".format(FlightNumber=FlightNumber, Airplane=Airplane, Airline=Airline, deptTime=deptTime, deptAirport=deptAirport, arrTime=arrTime, arrAirport=arrAirport, Price=Price, AvailableSeats=AvailableSeats, fid=fid)
+        SQL = "UPDATE flights SET FlightNumber = '{FlightNumber}', Airplane = '{Airplane}', Airline = '{Airline}', deptTime = '{deptTime}', deptAirport = '{deptAirport}', arrTime = '{arrTime}', arrAirport = '{arrAirport}', Price = '{Price}', AvailableSeats = {AvailableSeats} WHERE id = {fid};".format(
+            FlightNumber=FlightNumber, Airplane=Airplane, Airline=Airline, deptTime=deptTime, deptAirport=deptAirport,
+            arrTime=arrTime, arrAirport=arrAirport, Price=Price, AvailableSeats=AvailableSeats, fid=fid)
         print(SQL)
         mycursor.execute(SQL)
         mydb.commit()
         return True
 
+
 m = Manager()
-m.edit_flight(0, {'Price': 100, 'FlightNumber': 123, 'AvailableSeats': 20})
-# m.add_flight({'FlightNumber': 'HB1234', 'Airplane': 'Airbus 999', 'Airline': 'HB Airline', 'deptTime': '2021-09-01 12:25', 'deptAirport': 'CKX', 'arrTime': '2021-09-01 15:55', 'arrAirport': 'XYZ', 'Price': '9999', 'AvailableSeats': 120})
-m.delete_flight(6)
+# m.edit_flight(0, {'Price': 100, 'FlightNumber': 123, 'AvailableSeats': 20})
+m.add_flight(
+    {'FlightNumber': 'HB9999', 'Airplane': 'Airbus 999', 'Airline': 'HB Airline', 'deptTime': '2021-09-01 12:25',
+     'deptAirport': 'CKX', 'arrTime': '2021-09-01 15:55', 'arrAirport': 'XYZ', 'Price': '9999', 'AvailableSeats': 120})
+# m.delete_flight(6)
--- User.py
+++ User.py
@@ -5,26 +5,27 @@ from flask import Flask, render_template, request, url_for, redirect
 
 import hashlib
 
-import mysql.connector
+import pymysql
 
-mydb = mysql.connector.connect(
-    host = 'localhost',
-    port = 3306,
-    user = 'root',
-    passwd = 'david0811',
-    database = 'Airports',)
+mydb = pymysql.connect(
+    host='localhost',
+    port=3306,
+    user='root',
+    passwd='',
+    database='test', )
 
 mycursor = mydb.cursor()
 
-sql = "SELECT * FROM Flights WHERE carrier_name = 'Delta Air Lines' "
-mycursor.execute(sql)
-myresult = mycursor.fetchall()
+
+# sql = "SELECT * FROM Flights WHERE carrier_name = 'Delta Air Lines' "
+# mycursor.execute(sql)
+# myresult = mycursor.fetchall()
 
 
-app = Flask(__name__)
-Base = declarative_base()
-engine1 = create_engine('mysql+pymysql://root:david0811@localhost:3306/Airports')
-DBSession = sessionmaker(bind = engine1)
+# app = Flask(__name__)
+# Base = declarative_base()
+# engine1 = create_engine('mysql+pymysql://root:david0811@localhost:3306/Airports')
+# DBSession = sessionmaker(bind = engine1)
 
 class User():
     def __init__(self):
@@ -33,17 +34,51 @@ class User():
     def get_all(self):
         SQL = "SELECT * from flights"
 
-    def OrderByTime(self):
+    def QueryByLocationDate(self, fromLocation, toLocation, date, order="time"):
+        # Date: '2021-01-01'
+        # Location: 'PKX' (IATA code)
+        # Default ordering: order by deptTime
+        btime = date + ' 00:00'
+        etime = date + ' 23:59'
+
+        orderby = ""
+        if order == "time":
+            orderby = " ORDER BY deptTime"
+        if order == "price":
+            orderby = " ORDER BY Price DESC"
+
+        SQL = "SELECT * FROM flights WHERE deptAirport='{}' AND arrAirport='{}' AND deptTime BETWEEN '{}' AND '{}'{};".format(
+            fromLocation, toLocation, btime, etime, orderby)
+        print(SQL)
+        cursor = mydb.cursor()
+        cursor.execute(SQL)
+
+        result = cursor.fetchall()
+        return result
+
+    def QueryByFlightNum(self, flightNumber, date) -> dict:
+        # Assume each flight has EXACTLY ONE ENTRY
+        # Input: flightNumber
+        # Output: dict
+        btime = date + ' 00:00'
+        etime = date + ' 23:59'
+        SQL = "SELECT * FROM flights WHERE FlightNumber='{}' AND deptTime BETWEEN '{}' AND '{}'".format(flightNumber, btime, etime)
+        print(SQL)
+
+        cursor = mydb.cursor(cursor=pymysql.cursors.DictCursor)
+        cursor.execute(SQL)
+        flight = cursor.fetchall()
+        assert len(flight) == 1
+        # print(flight)
+        return flight[0]
+
+    def Book(self, primary_key):
+        # R, W
         pass
 
-    def OrderByPrice(self):
+    def UnBook(self, primary_key):
         pass
 
-    def QueryByFlightNum(self):
-        pass
 
-    def Book(self,primary_key):
-        pass
-
-    def UnBook(self,primary_key):
-        pass
+u = User()
+u.QueryByFlightNum("PN6213", "2021-08-12")
